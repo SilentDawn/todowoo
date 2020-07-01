1dd73375b0f84ef73aa3242b9e4ca24f195d476b
Andrew Simpson <39604738+SilentDawn@users.noreply.github.com>
2020-07-01 16:17:38 +1000

    Added basic user login

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
@@ -24,6 +24,16 @@ def signup_user(request):
 				return render(request, 'todo/signup_user.html', {'form':UserCreationForm(), 'error':'Username not available'})	
 		else:
 			return render(request, 'todo/signup_user.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+
+def login_user(request):
+	if request.method == 'GET':
+		return render(request, 'todo/login_user.html', {'form':AuthenticationForm()})
+	else:
+		user = authenticate(request, username = request.POST['username'].lower(), password = request.POST['password'])
+		if user is None:
+			return render(request, 'todo/login_user.html', {'form':AuthenticationForm(), 'error':'Invalid username/password'})
+		login(request, user)
+		return redirect('current_todos')
 	
 def logout_user(request):
 	if request.method == 'POST':
